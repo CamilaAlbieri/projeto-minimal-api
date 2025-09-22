0565cd343670f3f64b13f951ada22464b8615f71
CamilaAlbieri <camilamattos.mila@gmail.com>
2025-09-22 18:55:02 -0300

    Criando Seed para cadastrar administrador padrão no banco

--- Infrastructure/Db/DbContexto.cs
+++ Infrastructure/Db/DbContexto.cs
@@ -13,6 +13,18 @@ public class DbContexto : DbContext
     }
     public DbSet<Administrador> Administradores { get; set; } = default!;
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Administrador>().HasData(
+            new Administrador
+            {
+                Id = 1,
+                Email = "administrador@gmail.com ",
+                Senha = "administrador",
+                Perfil = "Adm"
+            }
+        );
+    }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         if (!optionsBuilder.IsConfigured)
@@ -20,7 +32,7 @@ public class DbContexto : DbContext
             var stringConexao = _configuracaoAppSettings.GetConnectionString("mysql")?.ToString();
             if (!string.IsNullOrEmpty(stringConexao))
             {
-                
+
                 optionsBuilder.UseMySql(
                     stringConexao,
                     ServerVersion.AutoDetect(stringConexao)
