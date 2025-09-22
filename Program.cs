85d8d89f034f662187d8cc947c249082cd46fab9
CamilaAlbieri <camilamattos.mila@gmail.com>
2025-09-22 18:35:56 -0300

    Configurando projeto com Entity Framework e Criando a tabela de Administradores no banco

--- Program.cs
+++ Program.cs
@@ -1,6 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalApi.Domain.DTOs;
+using MinimalApi.Infrastructure.Db;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DbContexto>(options =>
+{
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("mysql"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql"))
+    );
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Olá !");
 
+app.MapPost("/login", (LoginDTO loginDTO) => {
+    {
+    if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
+        return Results.Ok("Login feito com sucesso!");
+    }
+    return Results.Unauthorized();
+});
+
+
 app.Run();
+
